645912ee2e896068d36b462ce7891b0b8eae9e86
suraj <surajmore2143@.com>
2021-11-09 20:35:19 +0530

    MODIFIED Store Daily Wage along with the Total Wage

--- empwagecalculation.sh
+++ empwagecalculation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-#Constants
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HOUR=25
@@ -9,7 +8,6 @@ TOTAL_WORKING_DAYS=21
 MAX_HOURS_IN_MONTH=150
 
 
-#Variables
 totalEmpHrs=0
 totalWorkingDays=0
 totalSalary=0
