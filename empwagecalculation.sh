a89c6d378dddf13697e125dc9127ba95677c9870
suraj <surajmore2143@.com>
2021-11-09 18:01:11 +0530

    ADDED Using While Loop

--- empwagecalculation.sh
+++ empwagecalculation.sh
@@ -1,25 +1,39 @@
-#!/bin/bash
+#!/bin/bash -x
 
 
-isFullTime=1
-isPartTime=2
-empRatePerHr=25
-randomCheck=$((RANDOM%3))
+IS_FULL_TIME=1
+IS_PART_TIME=2
+EMP_RATE_PER_HOUR=25
+TOTAL_WORKING_DAYS=5
+MAX_HOURS_IN_MONTH=20
 
 
-if [ $isFullTime -eq $randomCheck ]
-then
-   workingHrs=8
-elif [ $isPartTime -eq $randomCheck ]
-then
-   workingHrs=4
-else
-   workingHrs=0
-fi
+totalEmpHrs=0
+totalWorkingDays=1
+totalSalary=0
 
 
-salary=$((workingHrs*empRatePerHr))
+while [[ $totalEmpHrs -lt $MAX_HOURS_IN_MONTH
+      && $totalWorkingDays -le $TOTAL_WORKING_DAYS ]]
+do
+   empCheck=$((RANDOM%3))
 
 
-echo "Working Hours: " $workingHrs
-echo "Salary is: " $salary
+   case $empCheck in
+      $IS_FULL_TIME) workingHrs=8 ;;
+      $IS_PART_TIME) workingHrs=4 ;;
+      *) workingHrs=0
+   esac
+
+
+   ((totalWorkingDays++))
+   totalEmpHrs=$((totalEmpHrs+workingHrs))
+done
+
+
+totalSalary=$((totalEmpHrs*EMP_RATE_PER_HOUR))
+
+
+echo "Total Working Days: " $totalWorkingDays
+echo "Total Working Hours: " $totalEmpHrs
+echo "Monthly Salary is: " $totalSalary
