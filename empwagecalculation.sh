0fdc46f7a22ae0d3a4751295c93b81718bbf9a53
suraj <surajmore2143@.com>
2021-11-09 19:46:23 +0530

    REFACTOR the code to function

--- empwagecalculation.sh
+++ empwagecalculation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 
-#Constants
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HOUR=25
@@ -9,7 +8,6 @@ TOTAL_WORKING_DAYS=5
 MAX_HOURS_IN_MONTH=20
 
 
-#Variables
 totalEmpHrs=0
 totalWorkingDays=1
 totalSalary=0
