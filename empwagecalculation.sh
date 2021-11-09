b2768a49dec9e169875ed86920b698c77533151b
suraj <surajmore2143@.com>
2021-11-09 18:47:41 +0530

    REFACTOR the code to function

--- empwagecalculation.sh
+++ empwagecalculation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 
+#Constants
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HOUR=25
@@ -8,14 +9,13 @@ TOTAL_WORKING_DAYS=5
 MAX_HOURS_IN_MONTH=20
 
 
+#Variables
 totalEmpHrs=0
 totalWorkingDays=1
 totalSalary=0
 
 
-while [[ $totalEmpHrs -lt $MAX_HOURS_IN_MONTH
-      && $totalWorkingDays -le $TOTAL_WORKING_DAYS ]]
-do
+function getWorkingHours() {
    empCheck=$((RANDOM%3))
 
 
@@ -24,6 +24,15 @@ do
       $IS_PART_TIME) workingHrs=4 ;;
       *) workingHrs=0
    esac
+}
+
+
+while [[ $totalEmpHrs -lt $MAX_HOURS_IN_MONTH
+      && $totalWorkingDays -le $TOTAL_WORKING_DAYS ]]
+do
+
+
+   getWorkingHours
 
 
    ((totalWorkingDays++))
